b55b961114ecc2f71ee5fe59eaa71d62e0f0fb1a
balatuluva <bala.tulva@gmail.com>
2024-02-18 09:01:43 +0000

    app module setup

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "test" {
+  ami= "ami-0f3c7d07486cad139"
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+}
+
+variable "instance_type" {}
+variable "subnet_id" {}
